3531ba4ec38280ee3268990e7eeb3a530cfc5b51
Sanket Phadtare <sanketphadtare324@gmail.com>
2025-04-19 20:11:20 +0530

    first commit v1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 // src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
